7d039f5fc168e07ba08a59cef4d2691d6644e3f5
Maël BABERT <m.babert@workstation3.nsi.lan>
2022-12-13 14:31:58 +0100

    ajout du setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Queue',
+    version='0.1',
+    url='https://github.com/babert-Mael/queue',
+    author='Babert Maël',
+    author_email='babert-m@saint-louis29.net',
+    description='A program to make queue in python',
+    packages=find_packages(),   
+    install_requires=[],
+)
